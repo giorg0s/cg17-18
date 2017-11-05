8b8d831bd304de71ba88906f029c913136875461
Jorge Guerrero <jgr1996@gmail.com>
2017-11-06 00:40:44 +0100

    Add files via upload

---
+++ cg_exercise_01/01_colors/build/CMakeFiles/cg.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/media/sf_shared/shared/cg_exercise_01/01_colors/src/exercise_01.cpp" "/media/sf_shared/shared/cg_exercise_01/01_colors/build/CMakeFiles/cg.dir/src/exercise_01.cpp.o"
+  "/media/sf_shared/shared/cg_exercise_01/01_colors/src/main.cpp" "/media/sf_shared/shared/cg_exercise_01/01_colors/build/CMakeFiles/cg.dir/src/main.cpp.o"
+  "/media/sf_shared/shared/cg_exercise_01/01_colors/src/renderers.cpp" "/media/sf_shared/shared/cg_exercise_01/01_colors/build/CMakeFiles/cg.dir/src/renderers.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "CGLIB_DIR=\"/media/sf_shared/shared/cg_exercise_01/cglib\""
+  "_USE_MATH_DEFINES"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/media/sf_shared/shared/cg_exercise_01/cglib/lib/glm"
+  "/media/sf_shared/shared/cg_exercise_01/cglib/src"
+  "/media/sf_shared/shared/cg_exercise_01/cglib/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/media/sf_shared/shared/cg_exercise_01/cglib/build/cglib/CMakeFiles/cglib_cg_1.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cg_exercise_01/01_colors/build/CMakeFiles/cg.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cg.dir/src/exercise_01.cpp.o"
+  "CMakeFiles/cg.dir/src/main.cpp.o"
+  "CMakeFiles/cg.dir/src/renderers.cpp.o"
+  "../cg.pdb"
+  "../cg"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cg.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
